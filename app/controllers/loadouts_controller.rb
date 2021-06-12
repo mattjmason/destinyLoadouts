d7ad17a66adf7a8cf7ff8491600038f9e522802d
mattjmason <masonmj10@gmail.com>
2021-06-12 01:01:19 -0400

    fixes

--- app/controllers/loadouts_controller.rb
+++ app/controllers/loadouts_controller.rb
@@ -1,12 +1,33 @@
 class LoadoutsController < ApplicationController
     get '/loadouts' do 
+        redirect_to_if_not_logged_in
         @user = current_user
         @loadouts = Loadout.all
         erb :'/loadouts/index'
     end
      
     get '/loadouts/new' do 
+        redirect_to_if_not_logged_in
         erb :'/loadouts/new'
+    end
+
+    post 'loadouts' do 
+        @loadout = current_user.loadouts.create(params)
+        if @loadout.save
+            redirect '/loadouts'
+        else
+            redirect '/loadouts/new'
+        end
+    end
+
+    get '/loadouts/:id' do 
+        @user = current_user
+        @loadout = Loadout.find_by_id(params[:id])
+        if @loadout
+            erb :'/loadouts/show'
+        else
+            redirect '/loadouts'
+        end
     end
 
 end
